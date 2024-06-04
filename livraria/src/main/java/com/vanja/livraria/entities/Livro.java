7457cc13846f22b23baf395b49de4fdbfceb8c9f
Nosliejnav <vanjeilsonm10@gmail.com>
2024-06-04 18:06:23 -0300

    Initial commit

---
+++ livraria/src/main/java/com/vanja/livraria/entities/Livro.java
@@ -0,0 +1,27 @@
+package com.vanja.livraria.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+   // private Autor autor;
+
+
+}
